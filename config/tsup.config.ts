2fc664859f7522985c2da72094894fbd7c6bd576
Alexander Smith <alexanderkingsmc@gmail.com>
2026-04-14 09:58:02 +0400

    fix: removed redundant code and installed new deps for copying js files on build

--- config/tsup.config.ts
+++ config/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/bin/cli.ts'],
+  entry: { 'bin/cli': 'src/bin/cli.ts' },
   format: ['esm'],
   target: 'node20',
   outDir: 'dist',
