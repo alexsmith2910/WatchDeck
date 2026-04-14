6e002736138d6b97685c3dc420df06b08f008344
Alexander Smith <alexanderkingsmc@gmail.com>
2026-04-14 08:40:51 +0400

    chore: project skeleton

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/bin/cli.ts'],
+  format: ['esm'],
+  target: 'node20',
+  outDir: 'dist',
+  clean: true,
+  sourcemap: true,
+  shims: false,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+})
